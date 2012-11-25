0de7b77c4a62ee0d0a8cb14e95fbd7d618577379
Bashlykevich <bashlykevich@gmail.com>
2012-11-26 00:52:46 +0200

    Старт проекта.

---
+++ OnlinerApp/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+using OnlinerApp.Rss;
+
+namespace OnlinerApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Refresh()
+        {
+            RefreshT();
+            RefreshA();
+            RefreshD();
+            RefreshN();
+        }
+
+        private void RefreshT()
+        {
+            string WindowsPhoneBlogPosts = "http://tech.onliner.by/feed";
+            RssService.GetRssItems(WindowsPhoneBlogPosts,
+                                    (items) => { listboxT.ItemsSource = items; },
+                                    (exception) => { MessageBox.Show(exception.Message); },
+                                    null);
+        }
+        private void RefreshA()
+        {
+            string WindowsPhoneBlogPosts = "http://auto.onliner.by/feed";
+            RssService.GetRssItems(WindowsPhoneBlogPosts,
+                                    (items) => { listboxA.ItemsSource = items; },
+                                    (exception) => { MessageBox.Show(exception.Message); },
+                                    null);
+        }
+        private void RefreshD()
+        {
+            string WindowsPhoneBlogPosts = "http://dengi.onliner.by/feed";
+            RssService.GetRssItems(WindowsPhoneBlogPosts,
+                                    (items) => { listboxD.ItemsSource = items; },
+                                    (exception) => { MessageBox.Show(exception.Message); },
+                                    null);
+        }
+        private void RefreshN()
+        {
+            string WindowsPhoneBlogPosts = "http://realt.onliner.by/feed";
+            RssService.GetRssItems(WindowsPhoneBlogPosts,
+                                    (items) => { listboxN.ItemsSource = items; },
+                                    (exception) => { MessageBox.Show(exception.Message); },
+                                    null);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+    }
+}
---
+++ ReadRssItemsSample/ReadRssItemsSample/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WindowsPhone.Helpers;
+
+namespace ReadRssItemsSample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private const string WindowsPhoneBlogPosts = "http://windowsteamblog.com/windows_phone/b/wpdev/rss.aspx";
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RssService.GetRssItems(
+                WindowsPhoneBlogPosts,
+                (items) => { listbox.ItemsSource = items; },
+                (exception) => { MessageBox.Show(exception.Message); },
+                null
+                );
+        }
+    }
+}
